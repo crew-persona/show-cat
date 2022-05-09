9b222dcc72f2afc05a754b731bb4ff635f6c805d
ForestLee0513 <woolimlee.dev@gmail.com>
2022-05-09 23:26:36 +0900

    fix: 전역 스타일에서 폰트가 적용 안되는 문제 개선

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -4,17 +4,18 @@ import { reset } from "stitches-reset";
 // Initialize Global Styles
 const globalStyles = globalCss({
   ...reset,
+
   "html, body": {
     width: "100%",
     height: "100%",
     "*": {
       fontFamily: "$default",
-      "@mobile": {
-        fontSize: "$small",
-      },
-      "@pc": {
-        fontSize: "$medium",
-      },
+    },
+    "@mobile": {
+      fontSize: "$small",
+    },
+    "@pc": {
+      fontSize: "$medium",
     },
   },
   // Heading
