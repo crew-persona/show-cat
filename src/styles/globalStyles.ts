574098e0884c2b9cbadddb78faa7b729261ba375
ForestLee0513 <woolimlee.dev@gmail.com>
2022-05-09 22:29:41 +0900

    update: Typography 스타일 지정

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -21,12 +21,29 @@ const globalStyles = globalCss({
   "h1, h2, h3, h4, h5, h6": {
     fontWeight: 600,
   },
-  h1: {},
-  h2: {},
-  h3: {},
-  h4: {},
-  h5: {},
-  h6: {},
+  h1: {
+    fontSize: "1.938rem",
+  },
+  h2: {
+    fontSize: "1.562rem",
+  },
+  h3: {
+    fontSize: "1.250rem",
+  },
+  h4: {
+    fontSize: "1rem",
+  },
+  // paragraph
+  ".p": {
+    fontSize: "1rem",
+  },
+  // paragraph
+  ".p-sm": {
+    fontSize: "0.812rem",
+  },
+  ".p-xs": {
+    fontSize: "0.625rem",
+  },
 });
 
 export default globalStyles;
