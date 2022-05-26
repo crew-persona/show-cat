1195290a8f57170520d093d4902ef573625ccc77
ForestLee0513 <woolimlee.dev@gmail.com>
2022-05-26 19:23:39 +0900

    feature: 헤더 레이아웃 적용

--- src/routes/display/Home.tsx
+++ src/routes/display/Home.tsx
@@ -1,9 +1,26 @@
+import Header, {
+  HeaderItem,
+  HeaderItemFull,
+  HeaderSubtitle,
+  HeaderTitle,
+} from "components/Header";
+import Button from "components/Button";
+import Container from "components/Container";
+
 export default function Home() {
   const uid = localStorage.getItem("uid");
   return (
-    <div>
-      Home
-      {uid && <span>UID: {uid}</span>}
-    </div>
+    <>
+      <Header>
+        <HeaderItemFull css={{ display: "flex", alignItems: "center" }}>
+          <HeaderTitle>이 주의 집사</HeaderTitle>
+          <HeaderSubtitle>매 주 진행되는 고양이 자랑 대회</HeaderSubtitle>
+        </HeaderItemFull>
+        <HeaderItem>
+          <Button color="primary">로그인</Button>
+        </HeaderItem>
+      </Header>
+      <Container></Container>
+    </>
   );
 }
