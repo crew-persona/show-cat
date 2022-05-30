c93d8b3ed86aefd6ad765a92408b1cc471ddf17b
ForestLee0513 <woolimlee.dev@gmail.com>
2022-05-30 19:23:05 +0900

    feature: 고양이 리스트 컴포넌트 추가

--- src/routes/display/Home.tsx
+++ src/routes/display/Home.tsx
@@ -6,6 +6,7 @@ import Header, {
 } from "components/Header";
 import Button from "components/Button";
 import Container from "components/Container";
+import { ListContainer, ListImage, ListItem } from "components/CatList";
 
 export default function Home() {
   const uid = localStorage.getItem("uid");
@@ -20,7 +21,28 @@ export default function Home() {
           <Button color="primary">로그인</Button>
         </HeaderItem>
       </Header>
-      <Container></Container>
+      <Container>
+        <ListContainer>
+          <ListItem count={{ "@pc": 4, "@mobile": 2 }}>
+            <ListImage
+              src={`https://images.unsplash.com/photo-1520315342629-6ea920342047?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2834`}
+              alt="cat"
+            />
+          </ListItem>
+          <ListItem count={{ "@pc": 4, "@mobile": 2 }}>
+            <ListImage
+              src={`https://images.unsplash.com/photo-1520315342629-6ea920342047?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2834`}
+              alt="cat"
+            />
+          </ListItem>
+          <ListItem count={{ "@pc": 4, "@mobile": 2 }}>
+            <ListImage
+              src={`https://images.unsplash.com/photo-1520315342629-6ea920342047?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2834`}
+              alt="cat"
+            />
+          </ListItem>
+        </ListContainer>
+      </Container>
     </>
   );
 }
