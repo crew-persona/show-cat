28d33c26953d435762cbf5d3c9f60d8b05f2341a
ForestLee0513 <woolimlee.dev@gmail.com>
2022-06-18 18:15:40 +0900

    update: 명예의 전당 Route 추가

--- src/routes/display/Home/BestCats.tsx
+++ src/routes/display/Home/BestCats.tsx
@@ -1,5 +1,28 @@
+import Text from "components/Text";
+import Heading from "components/Heading";
+import { ListContainer, ListImage, ListItem } from "components/CatList";
+
 const BestCats = () => {
-  return <h1>this is BestCats page</h1>;
+  return (
+    <>
+      <div className="title" style={{ marginBottom: "32px" }}>
+        <Heading as="h1" css={{ textAlign: "center", marginBottom: "12px" }}>
+          🏆 명예의전당
+        </Heading>
+        <Text size="sm" css={{ color: "$black4", textAlign: "center" }}>
+          일주일 동안 가장 인기 많았던 고양이 사진들이에요.
+        </Text>
+      </div>
+      <ListContainer>
+        <ListItem count={1}>
+          <ListImage
+            src={`https://images.unsplash.com/photo-1520315342629-6ea920342047?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2834`}
+            alt="cat"
+          />
+        </ListItem>
+      </ListContainer>
+    </>
+  );
 };
 
 export default BestCats;
--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -8,42 +8,6 @@ const globalStyles = globalCss({
   "html, body, #root": {
     width: "100%",
     height: "100%",
-    "*": {
-      fontFamily: "$default",
-    },
-    "@mobile": {
-      fontSize: "$small",
-    },
-    "@pc": {
-      fontSize: "$medium",
-    },
-  },
-  // Heading
-  "h1, h2, h3, h4, h5, h6": {
-    fontWeight: 600,
-  },
-  h1: {
-    fontSize: "1.938rem",
-  },
-  h2: {
-    fontSize: "1.562rem",
-  },
-  h3: {
-    fontSize: "1.250rem",
-  },
-  h4: {
-    fontSize: "1rem",
-  },
-  // paragraph
-  ".p": {
-    fontSize: "1rem",
-  },
-  // paragraph
-  ".p-sm": {
-    fontSize: "0.812rem",
-  },
-  ".p-xs": {
-    fontSize: "0.625rem",
   },
 });
 
