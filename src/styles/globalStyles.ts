f1fd81fd45ca3bcf1f9b9d162a5417caae2ed41b
ForestLee0513 <woolimlee.dev@gmail.com>
2022-06-23 15:29:32 +0900

    fix: 폰트 기본 크기 추가

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -8,6 +8,13 @@ const globalStyles = globalCss({
   "html, body, #root": {
     width: "100%",
     height: "100%",
+    fontFamily: "$default",
+    "@pc": {
+      fontSize: "16px",
+    },
+    "@mobile": {
+      fontSize: "14px",
+    },
   },
 });
 
